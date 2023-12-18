03840b70a76a62eb1bdf59388017cb33bee0046e
xandersalamander <xanderthehighlandersalamander@gmail.com>
2023-12-18 19:20:10 +0900

    VR-72 - DEV - Add enemy life bar

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,19 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float health = 100f;
+    public float maxHealth = 100f;
+
+    public GameObject healthBarUI;
+    public Slider slider;
 
     void Start()
     {
-        
+        health = maxHealth;
+        slider.value = CalculateHealth();
     }
 
     void Update()
     {
-        
+        slider.value = CalculateHealth();
+        // Activate health bar when hit
+        if (health < maxHealth)
+        {
+            healthBarUI.SetActive(true);
+        }
+        // Destroy when no health
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        // For healing
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    // 
+    float CalculateHealth()
+    {
+        return health / maxHealth;
     }
 
     // Take damage
