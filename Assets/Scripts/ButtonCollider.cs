a1870a0377643dff51d6b32e3ed47bb8ecbd2111
xandersalamander <xanderthehighlandersalamander@gmail.com>
2024-01-27 18:41:26 +0900

    VR-114 - Menu buttons need to be pressed for 2 seconds to activate

--- Assets/Scripts/ButtonCollider.cs
+++ Assets/Scripts/ButtonCollider.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,27 +6,46 @@ public class ButtonCollider : MonoBehaviour
 {
     public GameObject button;
     public AudioClip buttonPress;
-    public UnityEvent onPress;
     public UnityEvent onRelease;
+    public Color targetColor = Color.white;
+    public float pressDuration = 0.2f;
     private GameObject presser;
     private AudioSource audioSource;
     private bool isPressed;
+    private float pressStartTime;
+
+    private Color originalColor;
+    private Renderer buttonRenderer;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         isPressed = false;
+        // Ensure that the button has a renderer component
+        buttonRenderer = button.GetComponent<Renderer>();
+        if (buttonRenderer != null)
+        {
+            originalColor = buttonRenderer.material.color;
+        }
+        else
+        {
+            Debug.LogError("Button must have a Renderer component.");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!isPressed)
         {
+            // Pressed position
             button.transform.localPosition = new Vector3(0, 0.003f, 0);
             presser = other.gameObject;
-            onPress.Invoke();
+            // Play sound
             audioSource.PlayOneShot(buttonPress);
             isPressed = true;
+            pressStartTime = Time.time;
+            // Start the color transition on the button object
+            StartCoroutine(ChangeColorOverTime());
         }
     }
 
@@ -35,9 +53,37 @@ public class ButtonCollider : MonoBehaviour
     {
         if (other.gameObject == presser)
         {
+            // Original position
             button.transform.localPosition = new Vector3(0, 0.015f, 0);
-            onRelease.Invoke();
+            audioSource.Stop();
+            // Reset the button to original color
+            if (buttonRenderer != null)
+            {
+                buttonRenderer.material.color = originalColor;
+            }
+            // Collider is left after more than press duration
+            if (Time.time - pressStartTime > pressDuration)
+            {
+                // Call onRelease functions
+                onRelease.Invoke();
+            }
             isPressed = false;
+        }
+    }
+
+    // Coroutine to gradually change the color over time
+    IEnumerator ChangeColorOverTime()
+    {
+        float elapsedTime = 0f;
+        while (isPressed & elapsedTime < pressDuration)
+        {
+            buttonRenderer.material.color = Color.Lerp(originalColor, targetColor, elapsedTime / pressDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        if (isPressed)
+        {
+            buttonRenderer.material.color = targetColor;
         }
     }
 }
